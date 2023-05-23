ff8db01fefc961cc3a753ad03790702ec8a922e3
Kirylbas <kirill.kolobov.98@gmail.com>
2023-05-23 19:42:44 +0300

    initial commit

--- src/main/java/MoviesManager.java
+++ src/main/java/MoviesManager.java
@@ -3,33 +3,33 @@ public class MoviesManager {
     private int limit;
 
     public MoviesManager() {
-        this.limit =10;
+        this.limit = 10;
     }
 
 
     public void add(String movie) {
-        String[] tmp = new String[movies.length +1];
-        for (int i = 0; i < movies.length; i++){
+        String[] tmp = new String[movies.length + 1];
+        for (int i = 0; i < movies.length; i++) {
             tmp[i] = movies[i];
         }
         tmp[tmp.length - 1] = movie;
         movies = tmp;
     }
 
-    public String [] findAll(){
+    public String[] findAll() {
         return movies;
     }
 
-    public String [] findLast() {
+    public String[] findLast() {
         int resultLength;
-        if (movies.length < limit){
+        if (movies.length < limit) {
             resultLength = movies.length;
-        }else{
+        } else {
             resultLength = limit;
         }
         String[] tmp = new String[resultLength];
-        for (int i = 0; i < tmp.length; i++){
-            tmp[i] = movies[movies.length -1 - i];
+        for (int i = 0; i < tmp.length; i++) {
+            tmp[i] = movies[movies.length - 1 - i];
         }
         return tmp;
     }
